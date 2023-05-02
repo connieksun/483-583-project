eb5d34eedae695cb13b65d0358d01f000df65050
connieksun <71198240+connieksun@users.noreply.github.com>
2023-05-01 21:28:12 -0700

    update and re-run few shot .py

--- sentiment_classifier_cv.py
+++ sentiment_classifier_cv.py
@@ -1,105 +1,39 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-import sys
-
-print ('Argument List:', str(sys.argv))
-train_size = float(sys.argv[1])
-print()
-
-# In[1]:
-
-
 from torch import nn
 from transformers.modeling_outputs import SequenceClassifierOutput
 from transformers.models.bert.modeling_bert import BertModel, BertPreTrainedModel
 
-
-# In[2]:
-
-
 import torch
 torch.cuda.empty_cache()
 
-
-# In[3]:
-
-
-import sklearn
 import pandas as pd
 import numpy as np
-
-
-# In[4]:
-
+import os
 
 from transformers import AutoTokenizer
 from datasets import Dataset, DatasetDict
 from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
-from transformers import AutoConfig
-
-
-# In[5]:
-
-
-import os
-from os import listdir
-import sys
-import json
-from os import path
-
-
-# In[6]:
-
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 from sklearn import metrics
-
-
-# In[7]:
-
-
 from sklearn.model_selection import KFold
 
-
-# In[8]:
-
+import sys
+print ('Argument List:', str(sys.argv))
+train_size = float(sys.argv[1])
+print()
 
 models_dir = "models/bert-k-fold"
 
-
-# In[9]:
-
-
-import pandas as pd
 labels = open('data/classes.txt').read().splitlines()
 df = pd.read_csv("data/belief_benchmark_all_train.csv")
-
-
-# In[10]:
-
-
 df.head()
-
-
-# In[11]:
-
 
 transformer_name = "bert-base-cased"
 tokenizer = AutoTokenizer.from_pretrained(transformer_name)
 # NOTE: for cross validation, the model should be initialized inside the cv loop
 
-
-# In[12]:
-
-
 def tokenize(batch):
     return tokenizer(batch['text'], truncation=True)
-
-
-# In[13]:
-
 
 def compute_metrics(eval_pred):
     y_true = eval_pred.label_ids
@@ -111,13 +45,6 @@ def compute_metrics(eval_pred):
     
 
     return {'f1':metrics.f1_score(y_true, y_pred, average="macro")}
-
-
-# In[14]:
-
-
-# Note: not used right now, but can be
-# https://github.com/huggingface/transformers/blob/65659a29cf5a079842e61a63d57fa24474288998/src/transformers/models/bert/modeling_bert.py#L1486
 
 class BertForSequenceClassification(BertPreTrainedModel):
     def __init__(self, config):
@@ -149,17 +76,9 @@ class BertForSequenceClassification(BertPreTrainedModel):
             attentions=outputs.attentions,
         )
 
-
-# In[15]:
-
-
 # this is for creating cross-validation folds
 def get_sample_based_on_idx(data, indeces):
     return data.iloc[indeces, :].reset_index()
-
-
-# In[16]:
-
 
 # defining hyperparams
 num_epochs = 8
@@ -167,7 +86,7 @@ batch_size = 6
 weight_decay = 0.01
 print(f"num_epochs: {num_epochs}, batch_size: {batch_size}, weight_decay: {weight_decay}")
 training_args = TrainingArguments(
-    output_dir="./results_triggerless", # is this location in the tmp dir? 
+    output_dir="./results_sentiment_analysis",
     log_level='error',
     num_train_epochs=num_epochs,
     per_device_train_batch_size=batch_size,
@@ -179,23 +98,11 @@ training_args = TrainingArguments(
     metric_for_best_model="eval_f1" 
     )
 
-
-# In[17]:
-
-
 # specify what percent of the all_train data should be used in cross-fold validation 
 # we can use this to create a learning curve, e.g., performance with 50, 80, 90% of data
-
-from sklearn.model_selection import train_test_split
-# train_size = 0.8 # change to CLA
-
 if train_size < 1.0:
     df, _ = train_test_split(df, test_size=1.0-train_size, random_state=1, stratify=df[['label']])
 print(f"train_size: {train_size} (total samples: {len(df)})\n")
-
-
-# In[18]:
-
 
 output = open("original.txt", "a")
 
@@ -210,40 +117,30 @@ for train_df_idx, eval_df_idx in kfold.split(df):
     train_df = get_sample_based_on_idx(df, train_df_idx)
     print("LEN DF: ", len(train_df))
     output.write(f"LEN DF: {len(train_df)}\n")
-#     train_df['label'] = [int(item) for item in train_df["annotation: b (belief or attitude), n (not a belief and not an attitude)"]]
+
     print("done train df")
     output.write("done train df\n")
     eval_df = get_sample_based_on_idx(df, eval_df_idx)
-#     eval_df["label"] = [int(item) for item in eval_df['annotation: b (belief or attitude), n (not a belief and not an attitude)']]
+
     print("done eval df")
     output.write("done eval df\n")
     print("LEN EVAL: ", len(eval_df))
     output.write(f"LEN EVAL: {len(eval_df)}\n")
-#     print(eval_df.head())
+
     ds = DatasetDict()
     ds['train'] = Dataset.from_pandas(train_df)
     ds['validation'] = Dataset.from_pandas(eval_df)
     train_ds = ds['train'].map(
-        tokenize, batched=True,
-        # remove_columns=['index', 'sentence', 'trigger', 'annotation: b (belief or attitude), n (not a belief and not an attitude)', 'paragraph'],
+        tokenize, batched=True
     )
     eval_ds = ds['validation'].map(
         tokenize,
-        batched=True,
-        # remove_columns=['index', 'sentence', 'trigger', 'annotation: b (belief or attitude), n (not a belief and not an attitude)', 'paragraph'],
+        batched=True
     )
-
-#     config = AutoConfig.from_pretrained(
-#         transformer_name,
-#         num_labels=2,
-#     )
 
     model = AutoModelForSequenceClassification.from_pretrained(transformer_name, num_labels=4)
     tokenizer = AutoTokenizer.from_pretrained(transformer_name)
-#     model = (
-#         BertForSequenceClassification
-#         .from_pretrained(transformer_name, config=config)
-#     )
+
     trainer = Trainer(
         model=model,
         args=training_args,
@@ -277,10 +174,7 @@ for train_df_idx, eval_df_idx in kfold.split(df):
 
     print(f"************** END FOLD: {fold+1} **************\n")
     fold += 1
-        
 
-
-# In[19]:
 
 print("\n******************* holdout results ******************* ")
 holdout_df = df = pd.read_csv("data/belief_benchmark_holdout.csv")
@@ -304,18 +198,8 @@ import pickle
 pickle.dump(y_pred, open(f"few_shot_results/y_pred_{int(train_size*100)}", "wb"))
 pickle.dump(y_true, open(f"few_shot_results/y_true_{int(train_size*100)}", "wb"))
 
-# In[20]:
-
-
-import torch
 torch.cuda.empty_cache()
-
-
-# In[21]:
-
-
 torch.cuda.memory_summary(device=None, abbreviated=False)
 output.write("{torch.cuda.memory_summary(device=None, abbreviated=False)}")
 
 output.close()
-
